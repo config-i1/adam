0c71aaea74e9c183685310a40a0ea54e950d80d3
Ivan Svetunkov <ivan@svetunkov.ru>
2021-07-03 22:45:40 +0100

    pause before commiting changes

--- _build.sh
+++ _build.sh
@@ -6,5 +6,7 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+sleep 3
+
 git commit -am "New version of adam compiled"
 git push
