583b2a35406da913cddcc6e0127ca92d77cf8f1d
Ivan Svetunkov <ivan@svetunkov.ru>
2021-07-08 15:13:05 +0100

    Forgot some files

--- _build.sh
+++ _build.sh
@@ -6,8 +6,8 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
-sleep 3
+sleep 5
 
-git commit -am "New version of adam compiled. Step 1"
+git commit -am "New version of adam compiled"
 
 git push
