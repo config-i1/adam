6beb4c5aa124ecc45e053282791215c10c4319e8
Ivan Svetunkov <ivan@svetunkov.ru>
2021-07-27 21:56:56 +0100

    New version of adam compiled

--- _build.sh
+++ _build.sh
@@ -2,8 +2,8 @@
 
 set -ev
 
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
 sleep 3
