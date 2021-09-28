11de90b692b675907a7be835fab992d041f7e574
Ivan Svetunkov <ivan@svetunkov.ru>
2021-09-28 12:09:28 +0100

    New version of adam compiled

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ev
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
+sleep 3
+
+git commit -am "New version of adam compiled"
+
+git push
+
+git add * || true
+
+git commit -am "New version of adam compiled" || true
+
+git push
