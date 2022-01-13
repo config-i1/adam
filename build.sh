6ac70df39bdee7efc4c70f3ff45067735c82c632
Ivan Svetunkov <ivan@svetunkov.ru>
2022-01-13 14:30:41 +0000

    New version of adam compiled

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-mv _book/adam.pdf _book/Svetunkov\ \(2022\)\ \-\ ADAM.pdf
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', output_file='Svetunkov (2022) - ADAM.pdf')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_document2')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
