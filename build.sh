2badf43130281d1a2b672926ac53a42790b79c0b
Ivan Svetunkov <ivan@svetunkov.ru>
2022-01-13 14:08:23 +0000

    Experimenting with the name of file

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+mv _book/adam.pdf _book/Svetunkov\ \(2022\)\ \-\ ADAM.pdf
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', output_file='Svetunkov (2022) - ADAM.pdf')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_document2')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
